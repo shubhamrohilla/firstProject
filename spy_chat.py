1ad33669ff2e19f47923fd616aef31235aa30e58
shubham rohilla <shubhamrohilla61>
2017-08-04 02:42:14 +0530

    spy identity much imporved

--- spy_chat.py
+++ spy_chat.py
@@ -1,9 +1,17 @@
 print"Let's get Started !"
 spyname = raw_input("What is your name ? ")
-salutation = raw_input("You are Male or female ?" )
-if salutation.lower() == "male":
-    print 'Welcome MR.' + spyname +' Glad to have you back'
-elif salutation.upper() == "FEMALE":
-    print 'Welcome Miss. ' +spyname +' Glad to have you back'
+if len(spyname)>0:
+    salutation = raw_input("You are Male or female ?" )
+
+    if salutation.lower() == "male":
+        print 'Welcome MR.' + spyname +' Glad to have you back'
+    elif salutation.upper() == "FEMALE":
+        print 'Welcome Miss. ' +spyname +' Glad to have you back'
+    else:
+        print'Sorry ! Enter a Salutation First'
 else:
-    print'Sorry ! Select a Salutation First'
+    print "Dude What's wrong with you ? Please Enter your Name ! "
+
+
+
+
