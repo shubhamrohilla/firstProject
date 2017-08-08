493301bf5135e555f432301e4a2e3850f7fd4151
shubham rohilla <shubhamrohilla61>
2017-08-09 00:06:06 +0530

    identity with only character name in it

---
+++ practise.py
@@ -0,0 +1,6 @@
+number=int(raw_input("enter your number"))
+print "you have entered",number
+if (number%2)==0:
+    print 'number is evern'
+else:
+    print'number is odd'
--- spy_chat.py
+++ spy_chat.py
@@ -2,14 +2,19 @@ print"Let's get Started !"
 spyname = raw_input("What is your name ? ")
 spyname=" ".join(spyname.split())
 if len(spyname)>0:
-    salutation = raw_input("You are Male or female ?" )
-
-    if salutation.lower() == "male":
-        print 'Welcome MR.' + spyname +' Glad to have you back'
-    elif salutation.upper() == "FEMALE":
-        print 'Welcome Miss. ' +spyname +' Glad to have you back'
+    if spyname.isdigit():
+        print "You can not enter Numeric value in Name,please try again !"
     else:
-        print'Sorry ! Enter a Salutation First'
+        salutation = raw_input("You are Male or female ?")
+
+        if salutation.lower() == "male":
+            print 'Welcome MR.' + spyname + ' Glad to have you back'
+        elif salutation.upper() == "FEMALE":
+            print 'Welcome Miss. ' + spyname + ' Glad to have you back'
+        else:
+            print'Sorry ! Enter a Salutation First'
+
+
 else:
     print "Dude What's wrong with you ? Please Enter your Name ! "
 
